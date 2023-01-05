56e922f4d2887947f5cd1b52f8058951ebb04e0e
Ryan Hollink <76266509+LuaMeister@users.noreply.github.com>
2023-01-05 02:56:10 +0100

    basic create, inspect and remove reminder commands

--- extension.js
+++ extension.js
@@ -12,13 +12,14 @@ function activate(context) {
 	console.log("Remindr is now activated");
 
 	let addRecurringReminder = vscode.commands.registerCommand('remindr.addRecurringReminder', function () {
-		vscode.window.showInputBox({ title: "Name", placeHolder: "Stretch", prompt: "What is the Reminder's Name?" }).then((name) => {
+		vscode.window.showInputBox({ title: "Reminder Name", placeHolder: "Stretch", prompt: "What is the Reminder's Name?" }).then((name) => {
 			if (name == null || name == "") { return; }
 			vscode.window.showInputBox({ title: "Reminder", placeHolder: "It's time to stand-up and stretch!", prompt: "What do you want to be reminded of?" }).then((reminder) => {
 				if (reminder == null || reminder == "") { return; }
 				vscode.window.showInputBox({ title: "Interval", placeHolder: "20m", prompt: "How often do you want to be reminded?" }).then((interval) => {
 					if (interval == null || reminder == "") { return; }
 
+					context.globalState.update(name, JSON.stringify({message: reminder, interval: interval}))
 					// TODO: Create the actual Reminder
 					vscode.window.showInformationMessage(`Created ${name} reminder. You will be reminded every ${interval}`);
 				})
@@ -27,7 +28,15 @@ function activate(context) {
 	});
 
 	let removeReminder = vscode.commands.registerCommand('remindr.removeReminder', function () {
-		vscode.window.showInformationMessage("Not Implemented");
+		vscode.window.showInputBox({title: "Reminder Name", prompt: "Give the name of a Reminder that you would like to remove"}).then((name) => {
+			context.globalState.update(name, undefined);
+		})
+	})
+
+	let getReminder = vscode.commands.registerCommand('remindr.inspectReminder', function() {
+		vscode.window.showInputBox({title: "Reminder Name", prompt: "Give the name of a Reminder that you would like to inspect"}).then((name) => {
+			vscode.window.showInformationMessage(context.globalState.get(name));
+		})
 	})
 
 	context.subscriptions.push(addRecurringReminder);
