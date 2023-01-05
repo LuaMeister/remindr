f6ba1649606181d022df9ee71d7616c45f27f4c9
Ryan Hollink <76266509+LuaMeister@users.noreply.github.com>
2023-01-05 12:55:51 +0100

    cleaned up code

--- src/extension.js
+++ src/extension.js
@@ -1,16 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require('vscode');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log("Remindr is now activated");
-
 	let addRecurringReminder = vscode.commands.registerCommand('remindr.addRecurringReminder', function () {
 		vscode.window.showInputBox({ title: "Reminder Name", placeHolder: "Stretch", prompt: "What is the Reminder's Name?" }).then((name) => {
 			if (name == null || name == "") { return; }
