5671f383396dd66e1a41bf89b9441a1d333e7d87
Ryan Hollink <76266509+LuaMeister@users.noreply.github.com>
2023-01-05 02:29:21 +0100

    set up the command outline for addRecurringReminder

--- extension.js
+++ extension.js
@@ -12,18 +12,29 @@ function activate(context) {
 	console.log("Remindr is now activated");
 
 	let addRecurringReminder = vscode.commands.registerCommand('remindr.addRecurringReminder', function () {
-		vscode.window.showInformationMessage("Adding Reminder");
+		vscode.window.showInputBox({ title: "Name", placeHolder: "Stretch", prompt: "What is the Reminder's Name?" }).then((name) => {
+			if (name == null || name == "") { return; }
+			vscode.window.showInputBox({ title: "Reminder", placeHolder: "It's time to stand-up and stretch!", prompt: "What do you want to be reminded of?" }).then((reminder) => {
+				if (reminder == null || reminder == "") { return; }
+				vscode.window.showInputBox({ title: "Interval", placeHolder: "20m", prompt: "How often do you want to be reminded?" }).then((interval) => {
+					if (interval == null || reminder == "") { return; }
+
+					// TODO: Create the actual Reminder
+					vscode.window.showInformationMessage(`Created ${name} reminder. You will be reminded every ${interval}`);
+				})
+			})
+		});
 	});
 
-	let removeReminder = vscode.commands.registerCommand('remindr.removeReminder', function() {
-		vscode.window.showInformationMessage("Removing Reminder")
+	let removeReminder = vscode.commands.registerCommand('remindr.removeReminder', function () {
+		vscode.window.showInformationMessage("Removing Reminder");
 	})
 
 	context.subscriptions.push(addRecurringReminder);
 	context.subscriptions.push(removeReminder);
 }
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
