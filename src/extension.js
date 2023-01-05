76e7a9076b52a816304b38b3f71e4497f60e4179
Ryan Hollink <76266509+LuaMeister@users.noreply.github.com>
2023-01-05 13:38:21 +0100

    added some todo statements

--- src/extension.js
+++ src/extension.js
@@ -8,10 +8,15 @@ function activate(context) {
 	let addRecurringReminder = vscode.commands.registerCommand('remindr.addRecurringReminder', function () {
 		vscode.window.showInputBox({ title: "Reminder Name", placeHolder: "Stretch", prompt: "What is the Reminder's Name?" }).then((name) => {
 			if (name == null || name == "") { return; }
+
+			// make sure this name doesn't already exist
+
 			vscode.window.showInputBox({ title: "Reminder", placeHolder: "It's time to stand-up and stretch!", prompt: "What do you want to be reminded of?" }).then((reminder) => {
 				if (reminder == null || reminder == "") { return; }
 				vscode.window.showInputBox({ title: "Interval", placeHolder: "20m", prompt: "How often do you want to be reminded?" }).then((interval) => {
 					if (interval == null || reminder == "") { return; }
+
+					// TODO: Parse Interval before actually making the reminder
 
 					context.globalState.update(name, JSON.stringify({message: reminder, interval: interval}))
 					// TODO: Create the actual Reminder
